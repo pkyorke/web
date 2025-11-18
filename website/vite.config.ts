b94760467859e1161d6805b5c69f10eeb1695be2
pkyorke <pkyorke@gmail.com>
2025-11-18 03:42:03 -0800

    Update vite.config.ts

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
-const basePath = '/artist-portfolio/';
+const basePath = "/web/"; 
 
 export default defineConfig({
   base: basePath,
   plugins: [react()],
-  base: "/web/", 
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
