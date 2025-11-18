e92d087b6c81e5583f6c881c865dc581c45daf9f
pkyorke <pkyorke@gmail.com>
2025-11-18 14:29:46 -0800

    Update vite.config.ts

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 
-const basePath = "/web/"; 
-
 export default defineConfig({
-  base: basePath,
+  // For a custom domain (pkyorke.com), assets should be served from the root.
+  base: '/', 
   plugins: [react()],
   resolve: {
     alias: {
