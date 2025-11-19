e4dc7a92ab74399ea3d628733af179fbf862bb99
pkyorke <pkyorke@gmail.com>
2025-11-18 16:48:37 -0800

    Update AppShell.tsx

--- website/src/AppShell.tsx
+++ website/src/AppShell.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, Outlet, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
-import { motion } from "framer-motion";
 
 const navItems = [
   { to: "/", label: "Home" },
